7055f87870aa49c9c304b62644e610817289788b
Valeria Gutiérrez Aravena <valegutierrezaravena@gmail.com>
2025-09-18 18:12:42 -0300

    Adding service

--- src/main/java/com/duoc/atenciones/servicios/PacienteService.java
+++ src/main/java/com/duoc/atenciones/servicios/PacienteService.java
@@ -1,25 +1,60 @@
 package com.duoc.atenciones.servicios;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import com.duoc.atenciones.repositorios.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.duoc.atenciones.modelos.Paciente;    
 
 @Service
 public class PacienteService {
-    private List<Paciente> pacientes = new ArrayList<>();
-    private Long nextId = 1L;
 
-    // Crear un nuevo paciente
-    public Paciente crearPaciente(Paciente paciente) {
-        if (paciente.getId() == null || paciente.getId() == 0) {
-            throw new IllegalArgumentException("El ID del paciente no puede ser nulo o cero al crear un nuevo paciente.");
-        }
-        paciente.setId(nextId++);
-        pacientes.add(paciente);
-        return paciente;
+    @Autowired
+    private final PacienteRepository pacienteRepository;
+
+    public PacienteService(PacienteRepository pacienteRepository){
+        this.pacienteRepository = pacienteRepository;
     }
+
+    //Obtener todos los pacientes
+    public List<Paciente> obtenerPacientes(){
+        return pacienteRepository.findAll();
+    }
+
+    //Obtener un paciente por id
+    public Optional<Paciente> obtenerPorId(Long id){
+        return  pacienteRepository.findById(id);
+    }
+
+    //Guardar un nuevo paciente
+    public Paciente guardarPaciente(Paciente paciente){
+        return pacienteRepository.save(paciente);
+    }
+
+    //Editar o modificar un paciente por id
+    public ResponseEntity<Paciente> actualizarPaciente(Long id, Paciente datos){
+        return pacienteRepository.findById(id).map(paciente -> {
+            paciente.setNombre(datos.getNombre());
+            paciente.setApellido(datos.getApellido());
+            paciente.setRut(datos.getRut());
+            paciente.setFechaNacimiento(datos.getFechaNacimiento());
+            paciente.setEmail(datos.getEmail());
+            paciente.setTelefono(datos.getTelefono());
+            return ResponseEntity.ok(pacienteRepository.save(paciente));
+        }).orElse(ResponseEntity.notFound().build());
+    }
+    //Eliminar un paciente por id
+    public ResponseEntity<Void> eliminarPaciente(Long id){
+        if (pacienteRepository.existsById(id)){
+            pacienteRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
 
 }
