34cfbf58620f50f5ccee69a1c6f05e03463aa443
Valeria Gutiérrez Aravena <valegutierrezaravena@gmail.com>
2025-10-04 00:34:46 -0300

    Modifico controladores para uso libreria HATEOAS

--- src/main/java/com/duoc/atenciones/controladores/HistorialController.java
+++ src/main/java/com/duoc/atenciones/controladores/HistorialController.java
@@ -1,11 +1,14 @@
 package com.duoc.atenciones.controladores;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
-import com.duoc.atenciones.servicios.HistorialMedicoService;
+import com.duoc.atenciones.servicios.HistorialService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,36 +19,77 @@ import com.duoc.atenciones.modelos.HistorialMedico;
 public class HistorialController {
 
     @Autowired
-    private HistorialMedicoService historialMedicoService;
+    private HistorialService historialService;
 
     //ver todos
     @GetMapping
-    public List<HistorialMedico> verTodosLosHistoriales(){
-        return historialMedicoService.obtenerTodos();
+    public CollectionModel<EntityModel<HistorialMedico>> verTodosLosHistoriales(){
+        List<HistorialMedico> historiales = historialService.obtenerTodos();
+
+        List<EntityModel<HistorialMedico>> historialResources = historiales.stream()
+                .map(historialMedico -> EntityModel.of(historialMedico,
+                        WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).verHistorialPorId(historialMedico.getId())).withSelfRel()
+                ))
+                .collect(Collectors.toList());
+
+        WebMvcLinkBuilder linkTo = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).verTodosLosHistoriales());
+        CollectionModel<EntityModel<HistorialMedico>> resources = CollectionModel.of(historialResources, linkTo.withRel("historiales"));
+
+        return resources;
     }
+
+    /* public List<HistorialMedico> verTodosLosHistoriales(){
+        return historialMedicoService.obtenerTodos();
+    }*/
 
     //ver por id
     @GetMapping("/{id}")
-    public Optional<HistorialMedico> verPorId(@PathVariable Long id){
-        return historialMedicoService.obtenerPorId(id);
+    public EntityModel<HistorialMedico> verHistorialPorId(@PathVariable Long id){
+        Optional<HistorialMedico> historial = historialService.obtenerPorId(id);
+
+        if (historial.isPresent()) {
+            return EntityModel.of(historial.get(),
+                    WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).verHistorialPorId(id)).withSelfRel(),
+                    WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).verTodosLosHistoriales()).withRel("todos-los-historiales"));
+        } else {
+            throw new HistorialNotFoundException("Historial no encontrado con id: " + id);
+        }
     }
+
+   /* public Optional<HistorialMedico> verPorId(@PathVariable Long id){
+        return historialMedicoService.obtenerPorId(id);
+    }*/
 
     //guardar
     @PostMapping
-    public HistorialMedico guardarHistorial(@RequestBody HistorialMedico historialMedico){
-        return historialMedicoService.guardarHistorial(historialMedico);
+    public EntityModel<HistorialMedico> guardarHistorial(@RequestBody HistorialMedico historialMedico){
+        HistorialMedico historialGuardado = historialService.guardarHistorial(historialMedico);
+        return EntityModel.of(historialGuardado,
+                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).verHistorialPorId(historialGuardado.getId())).withSelfRel(),
+                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).verTodosLosHistoriales()).withRel("todos-los-historiales"));
     }
+
+   /* public HistorialMedico guardarHistorial(@RequestBody HistorialMedico historialMedico){
+        return historialMedicoService.guardarHistorial(historialMedico);
+    }*/
 
     //modificar
     @PutMapping("/{id}")
-    public ResponseEntity<HistorialMedico> actualizarHistorial(@PathVariable Long id, @RequestBody HistorialMedico historialMedico){
-        return historialMedicoService.actualizarHistorial(id, historialMedico);
+    public EntityModel<HistorialMedico> actualizarHistorial(@PathVariable Long id, @RequestBody HistorialMedico historialMedico){
+        HistorialMedico historialActualizado = historialService.actualizarHistorial(id, historialMedico);
+        return  EntityModel.of(historialActualizado,
+                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).verHistorialPorId(id)).withSelfRel(),
+                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).verTodosLosHistoriales()).withRel("todos-los-historiales"));
     }
+
+    /* public ResponseEntity<HistorialMedico> actualizarHistorial(@PathVariable Long id, @RequestBody HistorialMedico historialMedico){
+        return historialMedicoService.actualizarHistorial(id, historialMedico);
+    }*/
 
     //eliminar
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> eliminarHistorial(@PathVariable Long id){
-        return historialMedicoService.eliminarHistorial(id);
+        return historialService.eliminarHistorial(id);
     }
 
 }
--- src/main/java/com/duoc/atenciones/controladores/PacienteController.java
+++ src/main/java/com/duoc/atenciones/controladores/PacienteController.java
@@ -2,8 +2,12 @@ package com.duoc.atenciones.controladores;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,25 +23,67 @@ public class PacienteController {
 
     //ver todos los pacientes
     @GetMapping
-    public List<Paciente> obtenerPacientes() {
-        return pacienteService.obtenerPacientes();
+    public CollectionModel<EntityModel<Paciente>> obtenerTodosLosPacientes(){
+        List<Paciente> pacientes = pacienteService.obtenerPacientes();
+
+        List<EntityModel<Paciente>> pacienteResources = pacientes.stream()
+                .map(paciente -> EntityModel.of(paciente,
+                        WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).obtenerPacientePorId(paciente.getId())).withSelfRel()
+                )).collect(Collectors.toList());
+
+        WebMvcLinkBuilder linkTo = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).obtenerTodosLosPacientes());
+        CollectionModel<EntityModel<Paciente>> resources = CollectionModel.of(pacienteResources, linkTo.withRel("pacientes"));
+
+        return resources;
     }
+
+   /* public List<Paciente> obtenerPacientes() {
+        return pacienteService.obtenerPacientes();
+    }*/
 
     //ver por id
     @GetMapping("/{id}")
-    public Optional<Paciente> obtenerPacientePorId(@PathVariable Long id) {
+    public EntityModel<Paciente> obtenerPacientePorId(@PathVariable Long id){
+        Optional<Paciente> paciente = pacienteService.obtenerPorId(id);
+
+        if (paciente.isPresent()) {
+            return EntityModel.of(paciente.get(),
+                    WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).obtenerPacientePorId(id)).withSelfRel(),
+                    WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).obtenerTodosLosPacientes()).withRel("todos-los-pacientes"));
+        } else {
+            throw new PacienteNotFoundException("Paciente no encontrado con id: " + id);
+        }
+    }
+
+    /*public Optional<Paciente> obtenerPacientePorId(@PathVariable Long id) {
         return pacienteService.obtenerPorId(id);
-    }
+    }*/
 
+    //Guardar paciente
     @PostMapping
-    public Paciente guardarPaciente(@RequestBody Paciente paciente){
-        return pacienteService.guardarPaciente(paciente);
+    public EntityModel<Paciente> guardarPaciente(@RequestBody Paciente paciente){
+        Paciente pacienteGuardado = pacienteService.guardarPaciente(paciente);
+        return EntityModel.of(pacienteGuardado,
+                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).obtenerPacientePorId(pacienteGuardado.getId())).withSelfRel(),
+                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).obtenerTodosLosPacientes()).withRel("todos-los-pacientes"));
     }
 
+   /* public Paciente guardarPaciente(@RequestBody Paciente paciente){
+        return pacienteService.guardarPaciente(paciente);
+    }*/
+
+    //modificar paciente
     @PutMapping("/{id}")
-    public ResponseEntity<Paciente> editarPaciente(@PathVariable Long id, @RequestBody Paciente paciente){
-        return pacienteService.actualizarPaciente(id, paciente);
+    public EntityModel<Paciente> actualizarPaciente(@PathVariable Long id, @RequestBody Paciente paciente){
+        Paciente pacienteActualizado = pacienteService.actualizarPaciente(id, paciente);
+        return EntityModel.of(pacienteActualizado,
+                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).obtenerPacientePorId(id)).withSelfRel(),
+                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).obtenerTodosLosPacientes()).withRel("todos-los-paientes"));
     }
+
+   /* public ResponseEntity<Paciente> editarPaciente(@PathVariable Long id, @RequestBody Paciente paciente){
+        return pacienteService.actualizarPaciente(id, paciente);
+    }*/
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> eliminarPaciente(@PathVariable Long id){
